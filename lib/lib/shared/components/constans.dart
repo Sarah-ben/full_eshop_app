f518354bd56c77280288fdac1cdf3bea33523fcd
Sarah-ben <70490047+Sarah-ben@users.noreply.github.com>
2022-04-24 15:51:40 +0100

    Delete lib directory

--- lib/lib/shared/components/constans.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-
- getWidth(context)=>MediaQuery.of(context).size.width;
- getHeight(context)=>MediaQuery.of(context).size.height;
--- lib/lib/views/home/CollectionList.dart
+++
@@ -1,4 +0,0 @@
-import 'dart:ui';
-
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
